ec8c2e3c35c98bc8d2909d3346f946d197c272ff
Jupiter090 <56195972+Jupiter090@users.noreply.github.com>
2022-01-31 16:42:00 +0100

    Added new project

---
+++ my-projects/projects/redirect.js
@@ -0,0 +1,6 @@
+var redWebBtn = document.querySelector("#redirectwebsite")
+
+redWebBtn.addEventListener("click", () =>{
+    location.replace('https://www.github.com/Jupiter090/Calculator-App');
+
+})
