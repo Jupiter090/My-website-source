d59f95c3ea32b2c393d27da016f9ca3944ed4cf7
Jupiter090 <56195972+Jupiter090@users.noreply.github.com>
2022-04-08 21:28:58 +0200

    Added new project

---
+++ my-projects/projects/redirect-to-github.js
@@ -0,0 +1,6 @@
+var redWebBtn = document.querySelector("#redirectwebsitegt")
+
+redWebBtn.addEventListener("click", () =>{
+    location.replace('https://github.com/Jupiter090/Calculator-Windows-Form-App');
+
+})
